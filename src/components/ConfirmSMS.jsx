04ba1228dba571e6208b83ef483996807f5f6444
Esthuardo Quispe <quispefesthuardo@gmail.com>
2023-08-18 14:09:19 -0500

    actualizando confirmSMS

--- src/components/ConfirmSMS.jsx
+++ src/components/ConfirmSMS.jsx
@@ -1,24 +1,37 @@
-
-import React from 'react'
+import React, { useState } from 'react'
 import { useRef } from 'react'
 import { albumImagenesEstaticas } from '../assets/images/ImagenesStaticas'
 const ConfirmSMS = () => {
-  const codigoSMS = Array.from({ length: 5 }, (_, index) => useRef(null))
+  const [SMS,setSMS] = useState(['','','','',''])
+  const referencia = useRef([])
+
   const handleInput = (event, index)=>{
-    const codigo = codigoSMS[index]
-    if (event.target.value.length === 1 && codigo.current) {
-        codigo.current.focus();
+    const nuevoCodigo = [...SMS]
+    nuevoCodigo[index]=event.target.value
+    setSMS(nuevoCodigo)
+    if (event.target.value.length === 1 && index < referencia.current.length - 1) {
+        referencia.current[index+1].focus()
     }
   }
+  // Este codigo es temporal solo para confirmar que funcione adecuadamente
+  const CodigoTemporal = '12345'
+
   return (
     <>
-        <label className='ml-2 text-[1.328125rem] onlyLine' htmlFor="codigoSMS">Código SMS</label>
+        <label className='ml-2 text-[1.328125rem] onlyLine' htmlFor="SMS">Código SMS</label>
         <div className='flex gap-4 justify-center'>
-            {codigoSMS.map((codigo,index)=>(
-                <input  className='rounded-md h-10 w-8 border-2 border-black text-center' ref={codigo} maxLength={1} type='text' required onInput={event=>handleInput(event,index+1)}/>
+            {SMS.map((sms,index)=>(
+                <input
+                  key={index}
+                  className='rounded-md h-10 w-8 border-2 border-black text-center' 
+                  maxLength={1} 
+                  type='text' 
+                  required 
+                  onChange={event=>handleInput(event,index)}
+                  ref={elemento => referencia.current[index]=elemento}
+                  />
             ))}
-            {/* al escribir la funcion hace focus al input siguiente, por lo que el ref sirve para que el focus continue y no se mantenga en el inicial */}
-            <img className='h-12' src={albumImagenesEstaticas.confirmarSMS} alt="Confirmar SMS"/>
+            <img className='h-10' src={albumImagenesEstaticas.confirmarSMS} alt="Confirmar SMS"/>
         </div>
         {/* <input id='codigoSMS' className='rounded-[15px] h-10 w-[20rem] border-2 border-black pl-4' type="text" /> */}
     </>
